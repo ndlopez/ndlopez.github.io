41e8fe917edf80776d17e6dba51b65f2dc3c4a34
Diego Lopez <ndzerglink@live.jp>
2022-04-10 15:45:42 +0900

    Re-designed d3js plot

--- static/fetch_weather.js
+++ static/fetch_weather.js
@@ -56,10 +56,12 @@ async function display_info(){
         }
     });*/
     
+    
+    console.log(myData.hour[4],myData.temp[4],myData.humid[4]);
     /* D3js plot 
     Thanks to  https://www.tutorialsteacher.com/d3js/animated-bar-chart-d3    */
     const data=[];
-    data.push(myData.hour,myData.temp);
+    data.push(myData.hour,myData.temp,myData.humid,myData.wind,myData.windDir);
     /* this was the problem, the data was not transposed*/
     const trData =data[0].map((_,colIdx)=> data.map(row => row[colIdx]));
 
@@ -70,6 +72,7 @@ async function display_info(){
     const margin = 40;
     const xMax = xSize - margin*2;
     const yMax = ySize - margin*2;
+    const newVal = 0;
     // Append SVG Object to the Page
     const svg = d3.select("#weather_bar")
     .append("svg").attr("width",xSize).attr("height",ySize)
@@ -83,8 +86,8 @@ async function display_info(){
     .padding(0.2);
     
     svg.append("g")
-    .attr("transform", "translate(0," + yMax + ")")
-    .call(d3.axisBottom(x));
+    .attr("transform", "translate(0," + newVal + ")")
+    .call(d3.axisTop(x));
 
     // Y Axis
     const y = d3.scaleLinear()
@@ -95,7 +98,7 @@ async function display_info(){
     .call(d3.axisLeft(y));
 
     // Dots. It finally worked!! 2022-03-28 21:40
-    /*svg.append('g')
+    /*svg.append('g')+"&#176;"
     .selectAll("dot")
     .data(trData).enter()
     .append("circle")
@@ -110,7 +113,14 @@ async function display_info(){
     .data(trData).enter()
     .append("rect")
     .attr("class","bar")
-    /*.on("mouseover",function(d,i){
+    .attr("x",(d)=>{return x(d[0]);})
+    .attr("y",(d)=>{return y(d[1]);})
+    .attr("width",x.bandwidth())
+    .transition().ease(d3.easeLinear)
+    .duration(1500).delay((d,i)=>{return i*100;})
+    .attr("height",(d)=>{return yMax -y(d[1]);});
+    /*is pointless on mobile
+    .on("mouseover",function(d,i){
         d3.select(this).attr("class","highlight");
         d3.select(this)
         .transition().duration(400)
@@ -131,14 +141,45 @@ async function display_info(){
         .attr("height",(d)=>{return yMax - y(d[1]);})
         d3.selectAll(".val").remove();
     })*/
-    .attr("x",(d)=>{return x(d[0]);})
-    .attr("y",(d)=>{return y(d[1]);})
-    .attr("width",x.bandwidth())
-    .transition().ease(d3.easeLinear)
-    .duration(500).delay((d,i)=>{return i*50;})
-    .attr("height",(d)=>{return yMax -y(d[1]);});
     //.attr("fill", "#fff");
+    svg.append("g")
+    .selectAll(".txtHumid")
+    .data(trData).enter()
+    .append("text")
+    .attr("class","txtHumid")
+    .text(function(d){return d[2]+"%";})
+    .attr("text-anchor","middle")
+    .attr("x",function(d){
+        return x(d[0])+13;/*+13 */})
+    .attr("y",function(d){return y(d[1])+20;})
+    .attr("font-family","sans-serif")
+	.attr("font-size","11px");
 
+    svg.append("g")
+    .selectAll(".txtWind")
+    .data(trData).enter()
+    .append("text")
+    .attr("class","txtWind")
+    .text(function(d){return d[3]+"m/s";})
+    .attr("text-anchor","middle")
+    .attr("x",function(d){
+        return x(d[0])+13;})
+    .attr("y",function(d){return yMax + 10;})
+    .attr("font-family","sans-serif")
+	.attr("font-size","11px");
+
+    svg.append("g")
+    .selectAll(".txtWindDir")
+    .data(trData).enter()
+    .append("text")
+    .attr("class","txtWindDir")
+    .text(function(d){return d[4];})
+    .attr("text-anchor","middle")
+    .attr("x",function(d){
+        return x(d[0])+13;})
+    .attr("y",function(d){return yMax + 30;})
+    .attr("font-family","sans-serif")
+	.attr("font-size","11px");
 }
 
 function onMouseOver(d,i){
@@ -165,7 +206,8 @@ function onMouseOut(d,i){
 }
 /*Using chart.js do this https://www.chartjs.org/docs/latest/samples/area/radar.html */
 async function get_url_data(curr_hour){
-    const hour=[],temp=[],humid=[],wind=[];
+    const hour=[], temp=[], humid=[], wind=[], windDir = [];
+    //const weather=[], rainProb=[], rainMM=[];
     
     const response = await fetch(url);
     const data = await response.text();
@@ -177,12 +219,18 @@ async function get_url_data(curr_hour){
         if (this_weather[1] === curr_hour){
             curr_weather.push(this_weather);
         }
-        hour.push(parseInt(this_weather[1]));
-        temp.push(parseFloat(this_weather[3]));
-        humid.push(parseInt(this_weather[6]));
-        wind.push(parseInt(this_weather[7]));
+        if (this_weather[1] >= curr_hour){
+            hour.push(parseInt(this_weather[1]));
+            /*weather.push(this_weather[2]);*/
+            temp.push(parseFloat(this_weather[3]));
+            /*rainProb.push(parseInt(this_weather[4]));
+            rainMM.push(parseInt(this_weather[5]));*/
+            humid.push(parseInt(this_weather[6]));
+            wind.push(parseInt(this_weather[7]));
+            windDir.push(this_weather[8].replace(/"/g,""));
+        }
     });
-    return {curr_weather,hour,temp,humid,wind};
+    return {curr_weather,hour,temp,humid,wind,windDir};
 }
 function update(selVar){
     alert("Error","This is not the "+ selVar + " you r looking for.");
