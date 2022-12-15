8eb9c56fe9cd78cd2a00f172bf3481660141c6ff
Diego Lopez <ndzerglink@live.jp>
2022-12-15 12:41:11 +0900

    Error: Cannot display xray flux

--- static/plot_xrays.js
+++ static/plot_xrays.js
@@ -6,7 +6,7 @@ const margin = {top: 10, right: 30, bottom: 30, left: 60},
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-const svg = d3.select("#my_dataviz")
+const svg = d3.select("#xrays-long")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -15,23 +15,29 @@ const svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json(xrays_url,
-    function(d){
-        console.log(d);
-        return {date: d3.timeParse()(d.time_tag),value:d.value}
-    },
     function(data){
-         // Add X axis --> it is a date format
-        var x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
+        // Add X axis --> it is a date format xValue = d3.utcParse("%Y-%m-%d %H:%M:%S")(d.time_tag);
+        var xValue="";
+        const x = d3.scaleTime()
+        .domain(d3.extent(d3.map(data,(d)=>{return d.time_tag;})))
         .range([ 0, width ]);
         svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
-
+        
         // Add Y axis
-        var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
+        const y = d3.scaleLinear()
+        .domain([1e-8, 1e-2])
         .range([ height, 0 ]);
         svg.append("g")
         .call(d3.axisLeft(y));
+        svg.append("path")
+        .data(data)
+        .attr("fill","none")
+        .attr("stroke","steelblue")
+        .attr("stroke-width",1.5)
+        .attr("d",d3.line()
+            .x((d)=>{return x(d.time_tag);})
+            .y((d)=>{return y(d.flux);}));
+        //console.log(data);
     });
