06d58a6c418ceef7e2609b77477c7bd8d81cacb0
Diego Lopez <ndzerglink@live.jp>
2023-02-04 23:18:25 +0900

    Added options to genPass

--- static/passGen.js
+++ static/passGen.js
@@ -1,28 +1,30 @@
-const tot=10;
-function pswdgen(){
-    const nump = (tot)?(tot):(10);
-    const txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-    const num = "23456789";
-    const sym = "!#$%&()-_}{@][";
+const default_length=10;
+const txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const num = "23456789";
+const sym = "!#$%&()-_}{@][";
+
+function pswdgen(size){
+    const nump = (size)?(size):(default_length);
+    
     let mystr = num + txt + sym;
     // returns a number between 0 and 9
     //var num2=Math.floor(Math.random() * 10);
     let pasw="";let hole="";
     //var tot=txt.length+num.length+sym.length;
-    let ent1,ent2,ent3,i=0;
-    while (hole.length<nump) {  
+    let ent1,ent2,ent3;
+    while (hole.length < nump) {
         ent1=Math.floor(mystr.length*Math.random()*Math.random());
         //ent2=Math.floor(Math.random()*Math.random()*num.length);
         //ent3=Math.floor(Math.random()*Math.random()*sym.length);
         //var vec=[ent1,ent2,ent3];
-        pasw+=mystr.charAt( ent1 );
+        pasw += mystr.charAt(ent1);
         //pasw+=txt[ent1];//Should not be treated as array!!
         //pasw+=num.charAt( ent2 );
         //pasw+=sym.charAt( ent3 );
-        hole=pasw;i++;
+        hole = pasw;
     }
     //return pasw;	//pasw is a string!
-    const num2=hole.length;
+    const num2 = hole.length;
     /*ent2=0;
     while(pasw.length<nump){
         ent1=Math.floor(Math.random()*num2);
