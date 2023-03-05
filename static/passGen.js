c52a36596cc6d57229a1b957b042c71539a9a261
Diego Lopez <ndzerglink@live.jp>
2023-03-05 17:54:35 +0900

    Added async funcs to fetch words

--- static/passGen.js
+++ static/passGen.js
@@ -65,3 +65,37 @@ function copy_to_clipboard(){
     // console.log("yourPass",getText.innerText);
     alert("Generated password was copied to clipboard");
 }
+
+async function disp_sched(){
+    const thisDay = await get_url_data(urlSched,key,elem1,elem2);
+
+    var text ="";
+    for (let idx=0; idx < thisDay["myTime"].length;idx++){        
+        text += "<tr><td style='text-align:right;'>" + thisDay["myTime"][idx] + "</td><td>" + thisDay["myAct"][idx]+"</td></tr>";
+    };
+    document.getElementById("myday").innerHTML = text;
+    //console.log(text);
+}
+
+async function get_url_data(url,jsonKey,keyElm1,keyElm2){
+    const response = await fetch(url);
+    const data = await response.json();
+    const myTime = [];
+    const myAct = [];
+    if (jsonKey === "quotes"){
+        const randIdx = Math.floor(Math.random()*data[jsonKey].length);
+        const quote = data[jsonKey][randIdx][keyElm1];
+        const author = data[jsonKey][randIdx][keyElm2];
+        //console.log(data["quotes"][randIdx]["quote"]);
+        return {quote,author};
+    }else{
+        const table = data[jsonKey];
+        table.forEach(row => {
+            //console.log(row[keyElm1]);
+            myTime.push(row[keyElm1]);
+            myAct.push(row[keyElm2]);
+        });
+        return {myTime,myAct};
+    }
+    //console.log(rows);
+}
