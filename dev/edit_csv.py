95155223221593b5d5ad1d97a6559f12d2e68464
Diego Lopez <ndzerglink@live.jp>
2023-03-06 21:15:42 +0900

    Updated sunspot number data

--- dev/edit_csv.py
+++ dev/edit_csv.py
@@ -1,25 +1,30 @@
-'''Read local csv file and edit date to output better format'''
+'''Read local csv file and edit date to output better format
+Should request data from: 
+https://sidc.be/silso/DATA/SN_d_tot_V2.0.txt
+'''
 import csv
-path_file = "../data/sunspot_number_all.csv"
+path_file = "/home/diego/Downloads/SN_d_tot_V2.0.csv"
 
 newFile = []
 fields = ["date","spotNum"]
+startYear = 2007
 
-with open(path_file,mode="r",) as data_file:
-    csvFile = csv.reader(data_file)
-    auxArr = "";
+with open(path_file,mode="r") as data_file:
+    csvFile = csv.reader(data_file,delimiter=";")
+    auxArr = ""
     for lines in csvFile:
         if not lines[0] == "year":
-            newArr = []
-            auxArr = lines[0] + "-" + lines[1] + "-" + lines[2]
-            # my_data = dict(date = auxArr,value = lines[4].strip())
-            newArr.append(auxArr)
-            newArr.append(lines[4].strip())
-            newFile.append(newArr)
+            if int(lines[0]) > startYear:
+                newArr = []
+                auxArr = lines[0] + "-" + lines[1] + "-" + lines[2]
+                # my_data = dict(date = auxArr,value = lines[4].strip())
+                newArr.append(auxArr)
+                newArr.append(lines[4].strip())
+                newFile.append(newArr)
 
 print(newFile)
 
-outFile = "../data/sunspot_number.csv"
+outFile = "../data/sunspot_number2.csv"
 with open(outFile,"w",newline='') as new_file:
     write = csv.writer(new_file)
     write.writerow(fields)
