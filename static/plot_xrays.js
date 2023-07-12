20c8c3da6f4db92cde4fb1e676c6dc01c2e362f4
Diego Lopez <ndzerglink@live.jp>
2023-07-12 21:26:56 +0900

    On the way figure out xrays

--- static/plot_xrays.js
+++ static/plot_xrays.js
@@ -1,44 +1,58 @@
-/*https://services.swpc.noaa.gov/products/summary/solar-wind-speed.json*/
-/*GOES 16 xrays 01~0.8nm*/
+/*GOES 16 xrays 01~0.8nm
+
+https://observablehq.com/@d3/line-chart/2?intent=fork
+
+*/
 const xrays_url = "https://services.swpc.noaa.gov/json/goes/primary/xrays-3-day.json";
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const margin = {top: 10, right: 10, bottom: 30, left: 35},
+   width = 460 - margin.left - margin.right,
+   height = 450 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#xrays-long")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+ .append("svg")
+   .attr("width", width + margin.left + margin.right)
+   .attr("height", height + margin.top + margin.bottom)
+   .append("g")
+   .attr("transform",
+         "translate(" + margin.left + "," + margin.top + ")");
 
+let idx = 0; 
 d3.json(xrays_url,
-    function(data){
-        // Add X axis --> it is a date format xValue = d3.utcParse("%Y-%m-%d %H:%M:%S")(d.time_tag);
-        var xValue="";
-        const x = d3.scaleTime()
-        .domain(d3.extent(data,(d)=>{return d.time_tag;}))
-        .range([ 0, width ]);
-        svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-        
-        // Add Y axis
-        const y = d3.scaleLinear()
-        .domain([1e-8, 1e-2])
-        .range([ height, 0 ]);
-        svg.append("g")
-        .call(d3.axisLeft(y));
-        svg.append("path")
-        .data(data)
-        .attr("fill","none")
-        .attr("stroke","steelblue")
-        .attr("stroke-width",1.5)
-        .attr("d",d3.line()
-            .x((d)=>{return x(d.time_tag);})
-            .y((d)=>{return y(d.flux);}));
-        //console.log(data);
-    });
+ function(data){
+   // Add X axis --> it is a date format xValue = d3.utcParse("%Y-%m-%d %H:%M:%S")(d.time_tag);
+   // d3.time.format("%Y-%m-%d %H:%M:%S").parse;
+   const xValue = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
+   const x = d3.scaleTime()
+   .domain(d3.extent(data,(d,i)=>{ if (i%2 == 0) {
+     //console.log(i,xValue(d.time_tag)); 
+     return xValue(d.time_tag);}}))
+   .range([ margin.left, width - margin.right]);
+
+   svg.append("g")
+   .attr("transform", "translate(0," + height + ")")
+   .call(d3.axisBottom(x));
+   
+   const lines = d3.line()
+       .x((d,i)=>{if(i % 2 == 0) {
+         console.log(xValue(d.time_tag),x(xValue(d.time_tag)));
+         return x(xValue(d.time_tag));}})
+       .y((d,i)=>{if(i % 2 == 0) {
+         console.log(d.flux * 1e+8, y(d.flux * 1e+8));
+         return y(d.flux * 1e+8);}})
+   // Add Y axis
+   // d3.max(data,(d,i)=>{if(i%2==0){return d.flux*1e+8;}})
+   const y = d3.scaleLinear()
+   .domain([1, d3.max(data,(d,i)=>{if(i%2==0){return d.flux*1e+8;}})])
+   .range([height - margin.bottom, margin.top]);
+
+   svg.append("g")
+   .call(d3.axisLeft(y));
+   svg.append("path")
+   .attr("fill","none")
+   .attr("stroke","steelblue")
+   .attr("stroke-width",1.5)
+   .attr("d",lines(data));
+   //console.log("really?",data);
+ });
