6e77067a0eccde23739ca582554fbe33efa1a490
Diego Lopez <ndzerglink@live.jp>
2023-09-07 21:55:41 +0900

    Added another text into svg clock

--- static/analog.js
+++ static/analog.js
@@ -9,10 +9,11 @@ svgObj.style.setProperty('--start-minutes',currTime.getMinutes());
 svgObj.style.setProperty('--start-hours',currTime.getHours() % 12);
 
 let marks = "";
-for (let idx = 0; idx < 12; idx++) {
+for (let idx = 0; idx < 11; idx++) {
    marks+= '<line x1="15" y1="0" x2="16" y2="0" />';
 }
-let txt = '<text x="0" y="0" class="dateText">'+theseDays[currTime.getDay()]+ currTime.getDate()+'</text>';
+let txt = '<text x="0" y="0" class="dateText texty">'+theseDays[currTime.getDay()]+ currTime.getDate()+'</text>';
+let txt12 = '<text x="0" y="0" class="text12 texty">12</text>';
 let index = '<line x1="0" y1="0" x2="9" y2="0" class="hour" /><line x1="0" y1="0" x2="13" y2="0" class="minute" /><line x1="0" y1="0" x2="16" y2="0" class="seconds" /><circle cx="20" cy="20" r="0.7" class="pin" />';
-svgObj.innerHTML = '<circle cx="20" cy="20" r="19" /><g class="marks">' + marks + '</g>' + txt + index;
+svgObj.innerHTML = '<circle cx="20" cy="20" r="19" /><g class="marks">' + marks + '</g>' + txt + txt12 + index;
 //console.log(txt);
