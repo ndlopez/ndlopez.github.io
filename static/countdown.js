67edb6cd51e3242942a8924bf8d1850881f348a9
Diego Lopez <ndzerglink@live.jp>
2023-09-10 00:21:08 +0900

    Better use padStart instead of ifs

--- static/countdown.js
+++ static/countdown.js
@@ -35,18 +35,11 @@ const gina = setInterval(function() {
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="zenith"
-  if (hours < 10){
-    hours = `0${hours}`;
-  }
-  if (minutes < 10){
-      minutes = `0${minutes}`;
-  }
-  if (seconds < 10){
-      seconds = `0${seconds}`;
-  }
+  /*if (hours < 10){ hours = `0${hours}`;}
+  if (minutes < 10){ minutes = `0${minutes}`;}
+  if (seconds < 10){ seconds = `0${seconds}`;}*/
 
-  document.getElementById("zenith").innerHTML = days + " : " + hours + " : "
-  + minutes + " : " + seconds;
+  document.getElementById("zenith").innerHTML = `${days} : ${hours.toString().padStart(2, '0')} : ${minutes.toString().padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`;
     
   // If the count down is over, go to next counter
   if (distance < 0) {
