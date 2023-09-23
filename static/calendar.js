967d2da98bcaee78a13821cbf6360e93a0d8a323
Diego Lopez <ndzerglink@live.jp>
2023-09-23 23:12:23 +0900

    RM ternary ope by padStart

--- static/calendar.js
+++ static/calendar.js
@@ -4,7 +4,7 @@ const day_arr = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 const timeNow = new Date();
 let year = timeNow.getFullYear();
 let monty = timeNow.getMonth() + 1;
-let today = timeNow.getDate();
+const today = timeNow.getDate();
 console.log("today",monty,today);
 let feb_date = (year%4==0 && year%100 !=0)?29:28;
 if(year%400 == 0){feb_date = 29};
@@ -15,15 +15,14 @@ const months = [{month: "January",days:31},{month:"February",days:feb_date},
 {month:"March",days:31},{month:"April",days:30},{month:"May",days:31},{month:"June",days:30},{month:"July",days:31},{month:"August",days:31},{month:"September",days:30},{month:"October",days:31},{month:"November",days:30},{month:"December",days:31}];
 console.log(months[monty-1].month,months[monty-1].days);
 
-const m_display=(monty<10)?"0" + String(monty):monty;
-const last_day=new Date(year,monty-1,month_count[monty]).getDay();
-const first_day=new Date(year,monty-1,1).getDay();
+const last_day = new Date(year,monty-1,month_count[monty]).getDay();
+const first_day = new Date(year,monty-1,1).getDay();
 let w = 1;
 let d = first_day;
 //Markup
-var txt = "";
+let txt = "";
 txt+= '<h2 style="text-align:center;">' + monthy[monty] + '&emsp;' + year + '</h2>';
-txt+= '<table summary="' + year + '/' + m_display + '/" class="calendar month' + 
+txt+= '<table summary="' + year + '/' + String(monty).padStart(2, '0') + '/" class="calendar month' + 
 monty + '"><tr>';
 day_arr.forEach(element => {
     txt+= "<th>" + element + "</th>";    
@@ -32,22 +31,22 @@ txt+= '</tr><tr class="week1">';
 for(let j=0;j<first_day;j++){
     txt+='<td>&nbsp;</td>';
 }
-for(let i=1;i<=month_count[monty];i++){
-    if(d!=0 && (first_day+i)%7==1){
+for(let idx=1;idx<=month_count[monty];idx++){
+    if(d!=0 && (first_day+idx)%7==1){
         w++;d = 0;
         txt+='</tr>';
         txt+='<tr class="week' + w + '">';
     }
     
-    let i_display = (i<10)?"0" + String(i):i;
-    let day_count = (i%7==0)? Math.floor(i/7):Math.floor(i/7)+1;
+    //let i_display = (idx<10)?"0" + String(idx):idx;
+    let day_count = (idx%7==0)? Math.floor(idx/7):Math.floor(idx/7)+1;
     
-    if(i == today){
-        txt += '<td id="d' + year + m_display + i_display + '" class="' + 
-        day_arr[d] + day_count + ' date' + i + ' today">' + i + '</td>';
+    if(idx == today){
+        txt += '<td id="d' + year + String(monty).padStart(2, '0') + String(idx).padStart(2, '0') + '" class="' + 
+        day_arr[d] + day_count + ' date' + idx + ' today">' + idx + '</td>';
     }else{
-        txt+='<td id="d' + year + m_display + i_display + '" class="' +
-        day_arr[d] + day_count + ' date' + i + '">' + i + '</td>';
+        txt+='<td id="d' + year + String(monty).padStart(2, '0') + String(idx).padStart(2, '0') + '" class="' +
+        day_arr[d] + day_count + ' date' + idx + '">' + idx + '</td>';
     }
     d++;
 }
