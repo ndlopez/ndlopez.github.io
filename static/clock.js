42b21f8f2ded9613688921234c11f5559ef69159
Diego Lopez <ndzerglink@live.jp>
2023-09-30 23:41:28 +0900

    added new css var

--- static/clock.js
+++ static/clock.js
@@ -10,10 +10,11 @@ let marks = "";
 for (let idx = 0; idx < 12; idx++) {
    marks+= '<line x1="15" y1="0" x2="16" y2="0" />';
 }
-let txt = '<text x="0" y="0" class="dateText texty">'+theseDays[currTime.getDay()]+ currTime.getDate()+'</text>';
-let txt12 = '<text x="0" y="0" class="text12 texty dials">60</text>';
-let txt6 = '<text x="0" y="0" class="text6 texty dials">30</text>';
-let txt9 = '<text x="0" y="0" class="text9 texty dials">45</text>';
+let txt = '<text x="0" y="0" class="dateText texty">' + 
+theseDays[currTime.getDay()] + currTime.getDate() + 
+'</text><text x="0" y="0" class="text12 texty dials">60</text>' + 
+'<text x="0" y="0" class="text6 texty dials">30</text>' + 
+'<text x="0" y="0" class="text9 texty dials">45</text>';
 let index = '<line x1="0" y1="0" x2="9" y2="0" class="hour" /><line x1="0" y1="0" x2="13" y2="0" class="minute" /><line x1="0" y1="0" x2="16" y2="0" class="seconds" /><circle cx="20" cy="20" r="0.7" class="pin" />';
-svgObj.innerHTML = '<circle cx="20" cy="20" r="19" /><g class="marks">' + marks + '</g>' + txt + txt12 + txt6 + txt9 + index;
+svgObj.innerHTML = '<circle cx="20" cy="20" r="19" /><g class="marks">' + marks + '</g>' + txt + index;
 //console.log(txt);
