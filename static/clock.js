638ac48d410ea4ca6b7fc62d53373df2a71a78d4
Diego Lopez <ndzerglink@live.jp>
2023-10-01 21:30:08 +0900

    Added 2nd dial to clock js code

--- static/clock.js
+++ static/clock.js
@@ -15,6 +15,6 @@ theseDays[currTime.getDay()] + currTime.getDate() +
 '</text><text x="0" y="0" class="text12 texty dials">60</text>' + 
 '<text x="0" y="0" class="text6 texty dials">30</text>' + 
 '<text x="0" y="0" class="text9 texty dials">45</text>';
-let index = '<line x1="0" y1="0" x2="9" y2="0" class="hour" /><line x1="0" y1="0" x2="13" y2="0" class="minute" /><line x1="0" y1="0" x2="16" y2="0" class="seconds" /><circle cx="20" cy="20" r="0.7" class="pin" />';
-svgObj.innerHTML = '<circle cx="20" cy="20" r="19" /><g class="marks">' + marks + '</g>' + txt + index;
+let index = '<line x1="0" y1="0" x2="6" y2="0" class="seconds" /><line x1="0" y1="0" x2="9" y2="0" class="hour" /><line x1="0" y1="0" x2="13" y2="0" class="minute" /><circle cx="20" cy="20" r="0.7" class="pin" />';
+svgObj.innerHTML = '<circle cx="20" cy="20" r="19" /> <circle class="inner_dial" stroke-dasharray="2,2" cx="13" cy="13" r="7"/><g class="marks">' + marks + '</g>' + txt + index;
 //console.log(txt);
