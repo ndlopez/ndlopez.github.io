777ebc54e719ab557251983532f8e46d95d11cb0
Diego Lopez <ndzerglink@live.jp>
2024-01-05 18:00:44 +0900

    updated sZenith countdown

--- static/countdown.js
+++ static/countdown.js
@@ -8,16 +8,9 @@
   Data from https://rl.se/zenith-calendar
   Set the date in JST counting down to
   idx, Day, Date Hour Altitude
-  3, Fri, Feb 3, 2023 01:46 89.7
-  4, Sat, Feb 4, 2023 01:46 90.0
-  5, Sun, Feb 5, 2023 01:46 89.7
-  6, Tue, Nov 7, 2023 01:16:10 89.6
-  7, Wed, Nov 8, 2023 01:16:13 89.9
-  8, Thu, Nov 9, 2023 01:16:17 89.9
-  9, Fri, Nov 10, 2023 01:16:22 89.6
 */
 let idx = 0;
-const allDates=["Nov 7, 2023 01:16:10", "Nov 8, 2023 01:16:13", "Nov 9, 2023 01:16:17", "Nov 10, 2023 01:16:22"];
+const allDates=["Feb 3, 2024 01:46:08", "Nov 8, 2023 01:16:13", "Nov 9, 2023 01:16:17", "Nov 10, 2023 01:16:22"];
 let countDownDate = new Date(allDates[idx]).getTime();
 
 const gina = setInterval(function() {
