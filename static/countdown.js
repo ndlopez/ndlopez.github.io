94ce5c022c5d66a8f6914fc3b39c3c5c635cf693
Diego Lopez <ndzerglink@live.jp>
2024-01-10 22:04:33 +0900

    Bug: cannot get async zenith data

--- static/countdown.js
+++ static/countdown.js
@@ -11,17 +11,22 @@
 */
 let idx = 0;
 const csv_url="https://raw.githubusercontent.com/ndlopez/scrapped/main/data/solar_zenith.csv";
-const allDates=["Feb 3, 2024 01:46:08", "Nov 8, 2023 01:16:13", "Nov 9, 2023 01:16:17", "Nov 10, 2023 01:16:22"];
+//const allDates=["Feb 3, 2024 01:46:08", "Nov 8, 2023 01:16:13", "Nov 9, 2023 01:16:17", "Nov 10, 2023 01:16:22"];
+const allDates = get_csv();
+
 let countDownDate = new Date(allDates[idx]).getTime();
-get_csv();
+
 async function get_csv(){
+  const dates = [];
   const resp = await fetch(csv_url);
   const data = await resp.text();
   const rows = data.split('\n').slice(1);
   rows.forEach(row => {
     const this_date = row.split(',');
+    dates.push(this_date[0]);
   });
-  console.log("this_data",data);
+  // console.log("this_data",dates);
+  return dates;
 }
 const gina = setInterval(function() {
 
