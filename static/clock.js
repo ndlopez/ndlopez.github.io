38128cfe656ab0e312628c6bc69bb9cd31947ebe
Diego Lopez <ndzerglink@live.jp>
2024-01-21 21:23:26 +0900

    clock:Turned inner dial into smoke color

--- static/clock.js
+++ static/clock.js
@@ -14,7 +14,8 @@ let txt = '<text x="0" y="0" class="dateText texty">' +
 theseDays[currTime.getDay()] + currTime.getDate() + 
 '</text><text x="0" y="0" class="text12 texty dials">60</text>' + 
 '<text x="0" y="0" class="text6 texty dials">30</text>' + 
-'<text x="0" y="0" class="text9 texty dials">45</text>';
+'<text x="0" y="0" class="text9 texty dials">45</text>' + 
+'<text x="0" y="0" class="text3 texty dials">15</text>';
 let index = '<line x1="0" y1="0" x2="6" y2="0" class="seconds" /><line x1="0" y1="0" x2="9" y2="0" class="hour" /><line x1="0" y1="0" x2="13" y2="0" class="minute" /><circle cx="20" cy="20" r="0.7" class="pin" />';
 svgObj.innerHTML = '<circle cx="20" cy="20" r="19" /> <circle class="inner_dial" stroke-dasharray="2,2" cx="13" cy="13" r="7"/><g class="marks">' + marks + '</g>' + txt + index;
 //console.log(txt);
